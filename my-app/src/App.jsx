c038ce7575bc19212428f16191513370464d2cea
Iryna Antoniuk <antonjuk.iryna@gmail.com>
2024-01-02 16:29:39 +0200

    added SignUpBanner

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,13 @@
 import Header from './sections/Header/Header'
+import SignUpBanner from './sections/SignUpBanner/SignUpBanner'
+
+import './app.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+      <SignUpBanner />
     </div>
   )
 }
---
+++ my-app/src/sections/SignUpBanner/SignUpBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './styles.scss'
+import Button from '../../components/Button/Button'
+
+const SignUpBanner = () => {
+  return (
+    <section className="sign-up-banner">
+      <div className="sign-up-banner-content">
+        <h1>Test assignment for front-end developer</h1>
+
+        <span className="sign-up-banner-content-text">
+          What defines a good front-end developer is one that has skilled
+          knowledge of HTML, CSS, JS with a vast understanding of User design
+          thinking as they'll be building web interfaces with accessibility in
+          mind. They should also be excited to learn, as the world of Front-End
+          Development keeps evolving.
+        </span>
+        <Button>Sign up</Button>
+      </div>
+    </section>
+  )
+}
+
+export default SignUpBanner
